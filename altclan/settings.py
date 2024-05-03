4d9e328fc2a4adbcc7a7a65bc4bdac7623bf01e1
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-05-03 11:12:11 +0100

    SECRET_KEY<>

--- altclan/settings.py
+++ altclan/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-#ioqb6nvy+hwu5(%r@(dv!6zztc7vb*+altclbrandsapivhihr5*oe"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
