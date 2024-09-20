5292d16e29d5ac26490ec404fe83b87b9efa5f7c
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-09-20 17:17:02 -0400

    sd

--- altclan/settings.py
+++ altclan/settings.py
@@ -233,8 +233,6 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-    'http://altclan.store',
-    'https://altclan.store',
     'http://altclan.com',
     'https://altclan.com',
     'https://altclan-api-v1.onrender.com',
