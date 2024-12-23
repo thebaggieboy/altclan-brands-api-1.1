95927e48af4cbf4795db84f816f2e7e6e39eec2a
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-12-23 17:56:30 -0500

    installed psycopg2-binary

--- altclan/settings.py
+++ altclan/settings.py
@@ -181,7 +181,7 @@ SESSION_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ['https://*.altclan.com',   'https://api.cloudinary.com',  'https://altclan.com', 'altclan.com', 'https://altclan-api-v1.onrender.com', 'https://altclan-brands-api-1-1.onrender.com',  'http://localhost:8000','http://127.0.0.1:8000', 'http://localhost:3000','http://127.0.0.1:3000',]
+CSRF_TRUSTED_ORIGINS = ['https://*.altclan.com',   'https://api.cloudinary.com',  'https://altclan.com', 'altclan.com', 'https://altclan-api-v1.onrender.com', 'https://altclan-brands-api-1-1.onrender.com', 'altclan-brands-api-1-1.onrender.com',  'http://localhost:8000','http://127.0.0.1:8000', 'http://localhost:3000','http://127.0.0.1:3000',]
 
 REST_AUTH = {
     
