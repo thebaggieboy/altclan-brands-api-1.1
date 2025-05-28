bfbadc43d2a32e1a003e7722ddaa87a3d81e888e
Mofe Odujirin <thebaggieboy@protonmail.com>
2025-05-28 16:50:42 -0400

    ds

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -10,51 +10,32 @@ class UserHyperlinkedField(serializers.HyperlinkedRelatedField):
         return super().get_url(obj, view_name, request, format)
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserHyperlinkedField(
-        view_name='user-detail',
-        queryset=User.objects.all()
-    )
+
     
     class Meta:
         model = Order
-        fields = ['url', 'id', 'email', 'item', 'total', 'ref_code', 'date_created', 'status', 'quantity']
-        extra_kwargs = {
-            'url': {'view_name': 'order-detail'},
-        }
+        fields = [ 'id', 'email', 'item', 'total', 'ref_code', 'date_created', 'status', 'quantity']
+      
 
 class PaymentSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserHyperlinkedField(
-        view_name='user-detail',
-        queryset=User.objects.all()
-    )
+
     
     class Meta:
         model = Payment
-        fields = ['url', 'id', 'email', 'paystack_reference_number', 'amount', 'status', 'timestamp']
-        extra_kwargs = {
-            'url': {'view_name': 'payment-detail'},
-        }
-
+        fields = ['id', 'email', 'paystack_reference_number', 'amount', 'status', 'timestamp']
+    
 class CouponSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserHyperlinkedField(
-        view_name='user-detail',
-        queryset=User.objects.all()
-    )
+
     
     class Meta:
         model = Coupon
-        fields = ['url', 'id', 'email', 'code', 'amount', 'name', 'description', 
+        fields = ['id', 'email', 'code', 'amount', 'name', 'description', 
                  'minimum_purchase', 'usage_limit', 'usage_limit_per_user', 
                  'status', 'start_date', 'end_date']
-        extra_kwargs = {
-            'url': {'view_name': 'coupon-detail'},
-        }
+    
 
 class RefundSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserHyperlinkedField(
-        view_name='user-detail',
-        queryset=User.objects.all()
-    )
+
     order = serializers.HyperlinkedRelatedField(
         view_name='order-detail',
         queryset=Order.objects.all()
@@ -62,48 +43,33 @@ class RefundSerializer(serializers.HyperlinkedModelSerializer):
     
     class Meta:
         model = Refund
-        fields = ['url', 'id', 'email', 'order', 'reason', 'accepted', 'email']
-        extra_kwargs = {
-            'url': {'view_name': 'refund-detail'},
-        }
+        fields = ['id', 'email', 'order', 'reason', 'accepted', 'email']
+        
 
 class SalesSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserHyperlinkedField(
-        view_name='user-detail',
-        queryset=User.objects.all()
-    )
+
     
     class Meta:
         model = Sales
-        fields = ['url', 'id', 'email', 'revenue']
+        fields = ['id', 'email', 'revenue']
         extra_kwargs = {
             'url': {'view_name': 'sales-detail'},
         }
 
 class BankSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserHyperlinkedField(
-        view_name='user-detail',
-        queryset=User.objects.all()
-    )
+
     
     class Meta:
         model = Accounts
-        fields = ['url', 'id', 'email', 'bank_name']
-        extra_kwargs = {
-            'url': {'view_name': 'accounts-detail'},
-        }
+        fields = ['id', 'email', 'bank_name']
+      
 
 class CardSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserHyperlinkedField(
-        view_name='user-detail',
-        queryset=User.objects.all()
-    )
+
     
     class Meta:
         model = Cards
-        fields = ['url', 'id', 'email', 'card_holder']
-        extra_kwargs = {
-            'url': {'view_name': 'cards-detail'},
-        }
+        fields = ['id', 'email', 'card_holder']
+      
 
  
