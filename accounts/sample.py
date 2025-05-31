9946bcbae2718b2c141b166d662bcde9e700fb2d
Mofe Odujirin <thebaggieboy@protonmail.com>
2025-05-31 14:00:00 -0400

    nmn

--- accounts/sample.py
+++ accounts/sample.py
@@ -1,24 +1,4 @@
-
-from django.db.models.signals import post_save
-from django.conf import settings
-from django.dispatch import receiver
-from .models import Profile
- 
-from .models import AccountUser
- 
-from brands.models import UserBillingAddress, BillingAddress
-from django.core.mail import send_mail
-
-
-
-
-User = settings.AUTH_USER_MODEL
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        subject = 'Welcome to Altclan'
-
-        message = f"""
+f"""
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -345,12 +325,3 @@ def create_profile(sender, instance, created, **kwargs):
 </body>
 </html>
 """
-        email_from = 'noreply@altclan.com'
-        recipient_list = [instance.email]
-        
-        Profile.objects.create(user=instance)
-        print("New user profile has been created for ", instance.email)
-        # send email to new user
-        send_mail( subject, message, email_from, recipient_list )
-
- 
