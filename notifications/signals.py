46bc701c1d4e1114d9d323de26b4528587f749ab
Mofe Odujirin <thebaggieboy@protonmail.com>
2025-06-05 09:07:57 -0400

    made more changes to dashboard, by linking the create pages and list pages, adding api creation functions

--- notifications/signals.py
+++ notifications/signals.py
@@ -1,12 +1,10 @@
 # notifications/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 from .models import Notification
 from accounts.models import CustomUser
- 
 from reviews.models import Reviews
-
-
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
@@ -14,7 +12,8 @@ from asgiref.sync import async_to_sync
 @receiver(post_save, sender=CustomUser)
 def create_new_user_notification(sender, instance, created, **kwargs):
     if created:
-        Notification.objects.create(
+        # Create the notification
+        notification = Notification.objects.create(
             user=instance,
             sender=instance,
             notification_type='NEW ACCOUNT',
@@ -29,11 +28,11 @@ def create_new_user_notification(sender, instance, created, **kwargs):
             {
                 'type': 'send_notification',
                 'notification': {
-                    'id': instance.id,
+                    'id': notification.id,  # Use notification.id instead of instance.id
                     'message': f"Welcome to altclan {instance.email}, you can get started by uploading your products",
                     'is_read': False,
-                    'created_at': str(instance.created_at),
+                    'created_at': str(notification.created_at),  # Use notification.created_at
                     'target_url': f"/brands/profile/{instance.id}/"
                 }
             }
-        )
+         )
