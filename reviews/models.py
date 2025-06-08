d1904867b5e25b8f74cc33c7b3b28796bfff6f76
Mofe Odujirin <thebaggieboy@protonmail.com>
2025-06-08 12:39:45 -0400

    swd

--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.shortcuts import reverse # Create your models here.
+from django.shortcuts import reverse
 from django.conf import settings
 from django.utils.text import slugify
 from datetime import timezone
@@ -7,9 +7,8 @@ from django.contrib.postgres.fields import ArrayField
  
 User = settings.AUTH_USER_MODEL
 
-# Create your models here.
 class Reviews(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reviews', null=True, blank=True)  # Made optional
     email = models.CharField(max_length=250, blank=True, null=True)
     merchandise_id = models.IntegerField(blank=True, null=True)
     merchandise_slug = models.SlugField(max_length=250, blank=True, null=True)
@@ -20,9 +19,8 @@ class Reviews(models.Model):
     def __str__(self):
         return f'{self.email}'
 
- 
 class Ratings(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_ratings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_ratings', null=True, blank=True)  # Made optional
     email = models.CharField(max_length=250, blank=True, null=True)
     merchandise_name = models.CharField(max_length=250, blank=True, null=True)
     merchandise_id = models.CharField(max_length=250, blank=True, null=True)
