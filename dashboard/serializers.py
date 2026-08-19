619df46f76ac7452c21cec831efdfb77b67e281d
thebaggieboy <thebaggieboy@protonmail.com>
2026-08-19 22:03:50 +0100

    dd

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -4,4 +4,4 @@ from .models import *
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Customers
-        fields = ['id', 'full_name', 'email']
+        fields = ['id', 'brand_name', 'full_name', 'email', 'first_name', 'last_name', 'location', 'status', 'mobile_number', 'orders', 'last_order', 'date_created', 'total_amount_spent']
